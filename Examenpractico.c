177efe57ebcf7c1797b73979a707d40807745c1f
JeanMV52 <jeanmorales24@outlook.com>
2023-05-03 15:23:11 -0500

    Segunda version del programa con potencia de la fórmula

--- Examenpractico.c
+++ Examenpractico.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
-int main()
+#include<math.h>
+int main(int argc, char const *argv[])
 {
     
+    double num, dato, i, elevado;
+    printf("Por favor ingrese un numero entero positivo: ");
+    scanf("%d", &num);
+    for(i=0; i<=num; i++){
+        double elevado = pow(-1, num);
+        
+    }
+    printf("La potencia es %lf\n", elevado);
+    return 0;
 }

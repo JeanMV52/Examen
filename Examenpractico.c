9d81b23bcc4d7aff561094efafe2ba2a2528bbf8
JeanMV52 <jeanmorales24@outlook.com>
2023-05-03 15:33:15 -0500

    Version final del ejercicio

--- Examenpractico.c
+++ Examenpractico.c
@@ -3,13 +3,14 @@
 int main(int argc, char const *argv[])
 {
     
-    double num, dato, i, elevado;
+    double num, dato, i, elevado, resultado1, resultadof;//variables para el ejercicio//
     printf("Por favor ingrese un numero entero positivo: ");
     scanf("%d", &num);
     for(i=0; i<=num; i++){
-        double elevado = pow(-1, num);
-        
+        double elevado = pow(-1, num);//potencia//
+        resultado1= 2 * num + 1;//dividendo//
+        resultadof= elevado/resultado1;//resultado final del ejercicio//
     }
-    printf("La potencia es %lf\n", elevado);
+    printf("El resultado es %lf\n", resultadof);
     return 0;
 }
